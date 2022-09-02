a46b94c3680a9cb62f0b9b385fb2d70caef37294
Casey DeMattina <cdemattina@gmail.com>
2022-09-02 17:50:26 -0400

    working with req/res

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  
+  console.log('url:' + url, 'method:' + method);
+
+  if(url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if(url === '/message' && method === 'POST') {
+    res.write('stuff');
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write('<html>stuff</html>');
+  res.end();
+
 });
 server.listen(3000);
