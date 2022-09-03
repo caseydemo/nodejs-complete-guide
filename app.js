eb4410c03bff65384d0be067eefccb005eb39a6c
Casey DeMattina <cdemattina@gmail.com>
2022-09-03 09:43:55 -0400

    updating app.js

--- app.js
+++ app.js
@@ -19,18 +19,21 @@ const server = http.createServer((req, res) => {
   }
   if (url === "/message" && method === "POST") {
     const body = [];
-    req.on("data", (chunk) => {      
+    req.on("data", (chunk) => {
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];      
-      fs.writeFileSync("message.txt", message);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+
     });
-    
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+
+
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>stuff</html>");
