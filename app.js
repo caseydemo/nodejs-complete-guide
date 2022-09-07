c58259104533214e7c55075702f747eaaef628f0
Casey DeMattina <cdemattina@gmail.com>
2022-09-07 07:54:17 -0400

    removing routes file and reference in app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 const http = require("http");
-const routes = require('./routes'); // note - since this is local and not a global resourse add ./
 const server = http.createServer(routes);
 server.listen(3000);
