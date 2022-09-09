7f25621198ca5616fb238270b10f0a466492caf6
Casey DeMattina <cdemattina@gmail.com>
2022-09-09 07:48:30 -0400

    installed express and began working with it

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const http = require("http");
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
+
+// default path for use() is "/"
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>Muh butt itches - with a product</h1>");
+});
+
+// default path for use() is "/"
+app.use("/", (req, res, next) => {
+  console.log("in the SECOND middle ware lol");
+  res.send("<h1>Muh butt itches</h1>");
+});
+
+const server = http.createServer(app);
 server.listen(3000);
