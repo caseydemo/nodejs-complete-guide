9622c859b43e72b9decd476eed1398f8b1f00587
Casey DeMattina <cdemattina@gmail.com>
2022-09-12 08:14:55 -0400

    imported the parse-body package and starting to parse input

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const http = require("http");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+/* third party library that parses body for us */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  // first middleware
+  next();
+});
+
+app.use((req, res, next) => {
+  // second middleware
+  next();
+});
 
 // default path for use() is "/"
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Muh butt itches - with a product</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 // default path for use() is "/"
 app.use("/", (req, res, next) => {
-  console.log("in the SECOND middle ware lol");
   res.send("<h1>Muh butt itches</h1>");
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
