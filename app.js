23aa609507dc8fc99058590004bb5519ba8c5589
Casey DeMattina <cdemattina@gmail.com>
2022-09-13 07:53:32 -0400

    implementing express.Router, created routes dir with admin.js and shop.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 /* third party library that parses body for us */
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
 
 app.use((req, res, next) => {
   // first middleware
@@ -15,21 +19,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// default path for use() is "/"
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
-  );
-});
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use(shopRoutes);
 
-// default path for use() is "/"
-app.use("/", (req, res, next) => {
-  res.send("<h1>Muh butt itches</h1>");
-});
 
 app.listen(3000);
