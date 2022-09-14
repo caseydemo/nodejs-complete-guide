7d223e6cbeeac4b365f0ad1e0824290c528cdc78
Casey DeMattina <cdemattina@gmail.com>
2022-09-14 07:58:54 -0400

    added filtering to router moving the form utilities to the '/admin' dir

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 /* third party library that parses body for us */
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
-
-app.use((req, res, next) => {
-  // first middleware
-  next();
-});
-
-app.use((req, res, next) => {
-  // second middleware
-  next();
-});
-
-
+/* add a filter to the adminRoutes - '/admin' now only urls that start with '/admin/xxx' will be considered - like an if statement */
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Error - Page Not Found</h1>');
+})
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,12 @@ const router = express.Router();
 /* form */
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
+    "<form action='/admin/add-product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
   );
 });
 
 /* form action */
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
