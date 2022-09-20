2dffcb4746e42bb85bdad666715276927827fbe4
Casey DeMattina <cdemattina@gmail.com>
2022-09-20 16:59:21 -0400

    working with pug templating

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const express = require("express");
 const app = express();
+
+app.set('view engine', 'pug');
+
+
 const bodyParser = require("body-parser");
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
 /* third party library that parses body for us */
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/* add a filter to the adminRoutes - '/admin' now only urls that start with '/admin/xxx' will be considered - like an if statement */
-app.use('/admin', adminRoutes);
+/* add a filter to the adminData - '/admin' now only urls that start with '/admin/xxx' will be considered - like an if statement */
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
+const adminData = require('./admin');
 
 // default path for use() is "/"
-router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
